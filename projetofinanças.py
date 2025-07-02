c70f8997db2ee4c62372dd99b53001b975c6ad10
Yuri Bastos <138149873+Bastos26@users.noreply.github.com>
2025-07-02 14:17:31 +0100

    Update projetofinanças.py

--- projetofinanças.py
+++ projetofinanças.py
@@ -1,9 +1,2 @@
-import requests
-import json
-token="tncDfegoH942k84xhJdiMf"
-ativo=input('digite um ativo:')
-url = f'https://brapi.dev/api/quote/{ativo}?token={token}'
-response=requests.get(url)
-data=response.json()
-print(json.dumps(data, indent=4, ensure_ascii=False))
+
 
